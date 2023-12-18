78819afaf21a8ecc86a5d0a0b2f6ad475bbb7068
NTAGANIRA-Richard <144215198+NTAGANIRA-Richard@users.noreply.github.com>
2023-12-19 01:36:39 +0200

    Delete KigaliMotariCoperative/KigaliMotariCoperative directory

--- KigaliMotariCoperative/KigaliMotariCoperative/src/main/java/com/auca/kigalimotaricoperative/repostory/ImisanzuDao.java
+++
@@ -1,7 +0,0 @@
-package com.auca.kigalimotaricoperative.repostory;
-
-import com.auca.kigalimotaricoperative.model.Imisanzu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImisanzuDao extends JpaRepository<Imisanzu,Integer> {
-}
--- KigaliMotariCoperative/KigaliMotariCoperative/src/main/java/com/auca/kigalimotaricoperative/repostory/MotariDao.java
+++
@@ -1,7 +0,0 @@
-package com.auca.kigalimotaricoperative.repostory;
-
-import com.auca.kigalimotaricoperative.model.Motari;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MotariDao extends JpaRepository<Motari,Integer> {
-}
